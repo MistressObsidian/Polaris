35a50c0708c43b48415d489cece1ae6aa79a7bba
MistressObsidian <mistressobsidian@example.com>
2026-03-06 23:38:15 -0700

    update changes

--- cypress/e2e/hello.cy.js
+++ cypress/e2e/hello.cy.js
@@ -1,6 +1,6 @@
 describe('My First Test', () => {
     it('Visits the application', () => {
         cy.visit('/');
-    cy.title().should('include', 'Bank Swift');
+    cy.title().should('include', 'Base');
     });
 });
